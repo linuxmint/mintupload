5269fe8ba93623b71cc2c2f47991140db5ee9292
Philip Morrell <ubuntu.emorrp1@mamber.net>
2009-05-02 13:26:56 +0100

    moving mintUploader

--- usr/lib/linuxmint/mintUpload/mintUploadCore.py
+++ usr/lib/linuxmint/mintUpload/mintUploadCore.py
@@ -76,3 +76,133 @@ class spaceChecker:
 			if self.filesize > self.available:
 				raise FilesizeError(_("File larger than service's available space"))
 
+class mintUploader:
+	'''Uploads the file to the selected service'''
+
+	def __init__(self, service, file):
+		self.service = service
+		self.file = file
+		self.name = os.path.basename(self.file)
+		self.filesize = os.path.getsize(self.file)
+		self.so_far = 0
+
+		# Switch to required connect function, depending on service
+		self.upload = {
+			'MINT': self._ftp, # For backwards compatiblity
+			'FTP' : self._ftp,
+			'SFTP': self._sftp,
+			'SCP' : self._scp}[self.service['type']]
+
+	def _ftp(self):
+		'''Connection process for FTP services'''
+
+		if not self.service.has_key('port'):
+			self.service['port'] = 21
+		try:
+			# Attempting to connect
+			ftp = ftplib.FTP()
+			ftp.connect(self.service['host'], self.service['port'])
+			ftp.login(self.service['user'], self.service['pass'])
+			self.progress(self.service['type'] + " " + _("connection successfully established"))
+
+			# Create full path
+			for dir in self.service['path'].split(os.sep):
+				try:	ftp.mkd(dir)
+				except:	pass
+				ftp.cwd(dir)
+
+			f = open(self.file, "rb")
+			self.progress(_("Uploading the file..."))
+			ftp.storbinary('STOR ' + self.name, f, 1024, callback=self.asciicallback)
+			f.close()
+			ftp.quit()
+
+		except:
+			# Close any open connections before raising error
+			try:	f.close()
+			except:	pass
+			try:	ftp.quit()
+			except:	pass
+			raise
+
+	def getPrivateKey(self):
+		'''Find a private key in ~/.ssh'''
+		key_files = [home + '/.ssh/id_rsa', home + '/.ssh/id_dsa']
+		for f in key_files:
+			if os.path.exists(f):
+				return paramiko.RSAKey.from_private_key_file(f)
+
+	def _sftp(self):
+		'''Connection process for SFTP services'''
+
+		if not self.service['pass']:
+			rsa_key = self.getPrivateKey()
+			if not rsa_key:	raise ConnectionError(_("Connection requires a password or private key!"))
+		if not self.service.has_key('port'):
+			self.service['port'] = 22
+		try:
+			# Attempting to connect
+			transport = paramiko.Transport((self.service['host'], self.service['port']))
+			if self.service['pass']:
+				transport.connect(username = self.service['user'], password = self.service['pass'])
+			else:
+				transport.connect(username = self.service['user'], pkey = rsa_key)
+			self.progress(self.service['type'] + " " + _("connection successfully established"))
+
+			# Create full remote path
+			path = self.service['path']
+			try:	transport.open_session().exec_command('mkdir -p ' + path)
+			except:	pass
+
+			sftp = paramiko.SFTPClient.from_transport(transport)
+			self.progress(_("Uploading the file..."))
+			sftp.put(self.file, path + self.name)
+			sftp.close()
+			transport.close()
+
+		except:
+			# Close any open connections before raising error
+			try:	sftp.close()
+			except:	pass
+			try:	transport.close()
+			except:	pass
+			raise
+
+	def _scp(self):
+		'''Connection process for SCP services'''
+
+		try:
+			# Attempting to connect
+			scp_cmd = "scp " + self.file + " " + self.service['user'] + "@" + self.service['host'] + ':' + self.service['path']
+			scp = pexpect.spawn(scp_cmd)
+
+			if self.service['pass']:
+				scp.expect('.*password:*')
+				scp.sendline(self.service['pass'])
+
+			self.progress(self.service['type'] + " " + _("connection successfully established"))
+
+			scp.timeout = None
+			received = scp.expect(['.*100\%.*','.*password:.*',pexpect.EOF])
+			if received == 1:
+				scp.sendline(' ')
+				raise ConnectionError(_("Connection requires a password!"))
+
+			scp.close()
+
+		except:
+			# Close any open connections before raising error
+			try:	scp.close()
+			except:	pass
+			raise
+
+	def progress(self, message):
+		print message
+
+	def asciicallback(self, buffer):
+		self.so_far = self.so_far+len(buffer)-1
+		pct = float(self.so_far)/self.filesize
+		pct = int(pct * 100)
+		print "so far:", pct, "%"
+		return
+
