59ecd8248b7ebd45d76f6d79204e9abfa8c2a121
Philip Morrell <ubuntu.emorrp1@mamber.net>
2009-05-02 13:30:05 +0100

    fixing Core dependencies

--- usr/lib/linuxmint/mintUpload/mintUploadCore.py
+++ usr/lib/linuxmint/mintUpload/mintUploadCore.py
@@ -7,21 +7,16 @@
 # as published by the Free Software Foundation; Version 2
 # of the License.
 
-try:
-	import sys
-	import urllib
-	import ftplib
-	import os
-	import datetime
-	import gettext
-	import paramiko
-	import pexpect
-	import commands
-	from user import home
-	from configobj import ConfigObj
-except:
-	print "You do not have all the dependencies!"
-	sys.exit(1)
+import sys
+import urllib
+import ftplib
+import os
+import datetime
+import gettext
+import paramiko
+import pexpect
+from user import home
+from configobj import ConfigObj
 
 # i18n
 gettext.install("messages", "/usr/lib/linuxmint/mintUpload/locale")
