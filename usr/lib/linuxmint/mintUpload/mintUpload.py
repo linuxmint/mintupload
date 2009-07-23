bf52d11b1c9c3743942c223bb4cae39081d8f66e
Philip Morrell <mintupload.emorrp1@mamber.net>
2009-07-23 12:56:04 +0100

    yat

--- usr/lib/linuxmint/mintUpload/mintUpload.py
+++ usr/lib/linuxmint/mintUpload/mintUpload.py
@@ -528,7 +528,7 @@ class servicesWindow:
 		wTree.get_widget("dialog_edit_service").set_icon_from_file(self.iconfile)
 		wTree.get_widget("dialog_edit_service").show()
 		wTree.get_widget("button_ok").connect("clicked", self.edit, wTree, file)
-		wTree.get_widget("button_cancel").connect("clicked", self.close_window, wTree)
+		wTree.get_widget("button_cancel").connect("clicked", self.close_window, wTree.get_widget("dialog_edit_service"))
 
 		#i18n
 		wTree.get_widget("lbl_type").set_label(_("Type:"))
